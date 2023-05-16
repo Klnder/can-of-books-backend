23739df72156e76b0a133a7fe7ba1e74ae8ed742
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-05-16 17:18:44 +0100

    lab 2 done

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 mongoose.connect(process.env.DATABASE_URL);
 const Book = require("./models/book");
@@ -18,6 +19,15 @@ app.get("/test", (request, response) => {
 app.get("/books", async (require, response) => {
   const books = await Book.find();
   response.json(books);
+});
+app.post("/books", async (request, response) => {
+  console.log("book adding");
+  const newBook = await Book.create(request.body);
+  response.json(newBook);
+});
+app.delete("/books/:id", async (request, response) => {
+  const deletedBook = await Book.findByIdAndDelete(request.params.id);
+  response.json(deletedBook);
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
